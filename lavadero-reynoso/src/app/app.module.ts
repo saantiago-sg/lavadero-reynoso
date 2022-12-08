12b26e11709b3020aa93a3d2897ddd6718decc71
santiago <saantiagosg@gmail.com>
2022-12-07 21:22:19 -0300

    animations added

--- lavadero-reynoso/src/app/app.module.ts
+++ lavadero-reynoso/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { APP_ROUTING } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { APP_ROUTING } from './app.routing';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,7 +20,8 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTING
+    BrowserAnimationsModule,
+    APP_ROUTING,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- lavadero-reynoso/src/app/components/home/home.component.ts
+++ lavadero-reynoso/src/app/components/home/home.component.ts
@@ -1,9 +1,18 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  styleUrls: ['./home.component.less'],
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 0 }), 
+        animate(2000, style({opacity: 1}))
+      ]) 
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
